fe9e22256227a8c2bba92e69932a151b4ac46ded
S1mon <s1monp@mailbox.org>
2021-01-20 00:46:13 +0100

    birthdays.txt parser

--- WichtelMann/mainBot.py
+++ WichtelMann/mainBot.py
@@ -26,11 +26,15 @@ async def on_ready():
    global channel_bot_test
    global channel_geburtsdaten
    global channel_wanderwichtelanmeldung
+   global channel_wichtelBotBefehle
+   global birthdayList
 
    channel_wichtelchat = client.get_channel(796051477738684428)
    channel_bot_test = client.get_channel(796376465741840444)
    channel_geburtsdaten = client.get_channel(796393926826655754)
    channel_wanderwichtelanmeldung = client.get_channel(796390311168835607)
+   channel_wichtelBotBefehle = client.get_channel(796441844933460059)
+   birthdayList = []
 
 @client.event
 async def on_message(message):
@@ -40,6 +44,29 @@ async def on_message(message):
     if message.content == 'Hi':
         await message.channel.send("Ho")
 
+    # Nachrichten im Bot Befehle Channel
+    if message.channel == channel_wichtelBotBefehle:
+        
+        if(message.content == 'help'):
+            await message.channel.send("\nDie Befehle sind:\n\troll -> Startet die zufällige Ziehnung")
+        if(message.content == 'roll'):
+            with open('birthdays.txt', "r") as f:
+                lines = f.readlines()
+                print(lines)
+                for line in lines:
+                    name, birthday = line.split(':')
+                    #nameLst = []
+                    #birthdayLst = []
+                    #nameLst.append(name)
+                    #birthdayLst.append(birthday)
+                    print(line)
+                    print(name)
+                    print(birthday)
+                    birthdayList[0].append(name)
+                    birthdayList[1].append(birthday)
+                print(birthdayList)
+
+    # Geburtsdatum in Channel Geburtsdaten eingeben und in Textdatei speichern
     if message.channel == channel_geburtsdaten:
         if re.search(r"^[0-9]{2}\.[0-9]{2}\.$", message.content):
             date = message.content.split('.')
@@ -49,8 +76,15 @@ async def on_message(message):
                     await message.author.add_roles(get(message.author.guild.roles , name='Wichtel-Wanderer'))
                     with open("birthdays.txt", "a") as file:
                         file.write(str(message.author) + ":" + message.content + "\n")
+                else:
+                    await message.delete()
+            else:
+                await message.delete()
         else:
             await message.delete()
+
+    if message.content == 'get_channel_id':
+        print(message)
         
 
 @client.event
